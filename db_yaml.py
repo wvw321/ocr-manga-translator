e699c5565aac4d5e0c07350bd622b1c7899195d4
wvw321 <wvw321@yandex.ru>
2022-06-15 22:20:01 +0300

    удаление отдельного интерфейса craft_net, спользование craft_net  из библиотеки easyokr

--- db_yaml.py
+++ db_yaml.py
@@ -1,40 +1,130 @@
 import yaml
-from os import path as os
+import os.path
 from numpy import ndarray
 
 
 class DBYaml:
-    def __init__(self, path_img: str = None, ):
-        if path_img is not None:
-            split_file_path, _ = os.splitext(path_img)
+    @staticmethod
+    def _checking_exceptions(str_value: str, list_value: list):
 
-            self.path = split_file_path + ".yaml"
+        if not isinstance(list_value, list):
+            raise TypeError('list_value wrong type variable')
+
+        if list_value is None or len(list_value) == 0:
+            raise ValueError('list_value is empty')
+
+        if not isinstance(str_value, str):
+            raise TypeError('str_value wrong type variable')
+
+        if str_value is None:
+            raise ValueError('str_value variable is empty')
+
+    @staticmethod
+    def _damp_yaml(path: str, data, key: str = None):
+        if key is None:
+            with open(path, "w") as fl:
+                yaml.dump(data, fl,
+                          default_flow_style=False)
         else:
-            self.path = None
+            with open(path, "w") as fl:
+                yaml.dump({key: data}, fl,
+                          default_flow_style=False)
 
-    def text_box_damp(self, boxes: list):
-        if boxes is not None and len(boxes) != 0:
-            if self.path is not None:
-                with open(self.path, "w") as fl:
-                    key = 1
-                    dict_text_box = {}
-                    for box in boxes:
-                        if isinstance(box, ndarray):
-                            dict_text_box[str(key)] = box.tolist()
-                            key += 1
+    @staticmethod
+    def _overwriting_yaml(key: str, dict_data: dict, path: str):
 
-                    yaml.dump({"boxes_group": dict_text_box}, fl,
-                              default_flow_style=False)
-            else:
-                raise ValueError('path variable is empty')
+        _data: dict = DBYaml._load_yaml(path)
+        dict_data = {key: dict_data}
+        data = _data | dict_data
+        DBYaml._damp_yaml(path=path,
+                          data=data)
 
-    def text_box_load(self, file_path: str):
-        with open(file_path) as file:
+    @staticmethod
+    def _load_yaml(path_yaml: str):
+        with open(path_yaml) as file:
             data = yaml.load(file, Loader=yaml.FullLoader)
-            box = data["boxes_group"]
-        return box
+        return data
+
+    @staticmethod
+    def from_image_path_to_yaml_path(path_img: str):
+        split_file_path, _ = os.path.splitext(path_img)
+        path = split_file_path + ".yaml"
+        return path
+
+    class damp:
+        def __init__(self, path: str, data, kay: str = None):
+            DBYaml._damp_yaml(path=path, key=kay, data=data)
+
+        @staticmethod
+        def _dict_create(dates):
+            count = 1
+            _dict_text_box = {}
+            for data in dates:
+                if isinstance(data, ndarray):
+                    _dict_text_box[str(count)] = data.tolist()
+                    count += 1
+                    continue
+                if isinstance(data, list) or isinstance(data, str):
+                    _dict_text_box[str(count)] = data
+                    count += 1
+            return _dict_text_box
+
+        @staticmethod
+        def text_box(path: str, boxes: list):
+            DBYaml._checking_exceptions(str_value=path, list_value=boxes)
+            key = "boxes_group"
+            dict_data = DBYaml.damp._dict_create(boxes)
+            if os.path.exists(path):
+                DBYaml._overwriting_yaml(key=key,
+                                         dict_data=dict_data,
+                                         path=path)
+            else:
+                DBYaml._damp_yaml(data=dict_data,
+                                  path=path,
+                                  key=key)
+
+        @staticmethod
+        def text(path: str, text_list: list):
+            DBYaml._checking_exceptions(str_value=path, list_value=text_list)
+            key = "text"
+            dict_data = DBYaml.damp._dict_create(text_list)
+            if os.path.exists(path):
+                DBYaml._overwriting_yaml(path=path,
+                                         key=key,
+                                         dict_data=dict_data,
+                                         )
+            else:
+                DBYaml._damp_yaml(path=path,
+                                  key=key,
+                                  data=dict_data)
+
+    class load:
+        def __init__(self, path_yaml: str):
+            DBYaml._load_yaml(path_yaml=path_yaml)
+
+        @staticmethod
+        def _load(key: str, path_yaml: str):
+            if os.path.exists(path_yaml):
+                data = DBYaml._load_yaml(path_yaml)
+                try:
+                    data = data[key]
+                    return data
+                except KeyError:
+                    print("variable '" + key + "' not found in yaml file")
+                    return False
+
+        @staticmethod
+        def text_box(path_yaml: str):
+            box = DBYaml.load._load("boxes_group", path_yaml)
+            return box
+
+        @staticmethod
+        def text(path_yaml: str):
+            text = DBYaml.load._load("text", path_yaml)
+            return text
 
 
 if __name__ == '__main__':
-    a = DBYaml().text_box_load(file_path="example/4.yaml")
-    print(a)
+    a = DBYaml.load.text_box(path_yaml="example/4.yaml")
+    b = DBYaml.load.text(path_yaml="example/4.yaml")
+    print(a, b)
--- imghandler.py
+++ imghandler.py
@@ -1,36 +1,48 @@
 import os.path
 import time
-from dataclasses import dataclass
-from config import IMAGE_DIRECTORY, CRAFT_PATH
+from collections import defaultdict
+
+import cv2
 import easyocr
-from cv2 import imread
+from sklearn.cluster import DBSCAN
 
 from craft_net_interface import CraftInterface
 from db_yaml import DBYaml
+from config import IMAGE_DIRECTORY
 
-
-@dataclass
-class Data:
-    box = dict()
-    text = dict()
-    translate = dict()
+from PIL import ImageDraw, Image
 
 
 class ImageHandler:
 
     def __init__(self,
                  image_directory: str = None,
-                 craft_net_path: str = None,
                  ):
 
         if isinstance(image_directory, str) is False:
             raise ValueError
-        self.image_directory = image_directory
 
-        self.craft_net_path = os.path.normpath(craft_net_path)
+        self.image_directory = image_directory
         self.img_names: list
         self.text_detector_model_craft_net = CraftInterface()
-        self.easyocr_model = None
+
+    @staticmethod
+    def cut_text_boxes(path_yaml: str, path_img: str):
+        if os.path.exists(path_yaml):
+            text_box = DBYaml.load.text_box(path_yaml=path_yaml)
+            img = cv2.imread(path_img)
+
+            circumcised_img_text = []
+            for key in text_box:
+                x1 = text_box[key][0]
+                x2 = text_box[key][2]
+                y1 = text_box[key][1]
+                y2 = text_box[key][3]
+                crop = img[y1:y2, x1:x2]
+                circumcised_img_text.append(crop)
+
+            return circumcised_img_text
+        return False
 
     def _links_loader(self):
         #  является ли путь директорией.
@@ -45,39 +57,122 @@ class ImageHandler:
         else:
             print("не верная директория ")
 
-    def _text_detector_craft_net(self, image_path: str):
+    def get_text_box(self, path_img: str = None):
+        reader = easyocr.Reader(['en'])
 
-        if self.text_detector_model_craft_net.net is None:
-            self.text_detector_model_craft_net.net_initialization(
-                    trained_model_path=self.craft_net_path
-            )
-        self.text_detector_model_craft_net.get_boxes(image_path=image_path)
-        boxes = self.text_detector_model_craft_net.bounding_boxes. \
-            depleted_regions
+        conf_DBSCAN = {'eps'        : 110,
+                       'min_samples': 2}
 
-        return boxes
+        conf_craftNet = {"text_threshold": 0.6,
+                         "link_threshold": 0.3,
+                         "canvas_size"   : 1280,
+                         "mag_ratio"     : 1.5,
+                         "low_text"      : 0.4,
+                         }
 
-    def _text_reader_easyocr(self, img):
-        if self.easyocr_model is None:
-            self.easyocr_model = easyocr.Reader(['en'])
-        bounds = self.easyocr_model.readtext(img)
-        return bounds
+        def _get_text_box_easyocr(img_path: str):
+            box_list = list()
+            clusters_boxes = defaultdict(list)
+            depleted_regions = list()
 
-    def get_text_box(self):
-        self._links_loader()
-        for img_name in self.img_names:
-            path_img = self.image_directory + "/" + img_name
-            boxes = self._text_detector_craft_net(image_path=path_img)
-            DBYaml(path_img=path_img).text_box_damp(boxes)
+            def _test_box_from_net():
+                nonlocal box_list
+                box_list = reader.detect(img_path, **conf_craftNet)
+                box_list = box_list[0][0]
 
-    def get_text(self, method: str = "easyocr"):
+            def _clustered_text_box():
+                nonlocal clusters_boxes
+                clustered = DBSCAN(**conf_DBSCAN).fit_predict(box_list)
 
-        for img_name in self.img_names:
-            split_name, _ = os.path.splitext(img_name)
-            path_yaml = self.image_directory + "/" + split_name + ".yaml"
-            path_img = self.image_directory + "/" + img_name
-            text_box = DBYaml().text_box_load(file_path=self.image_directory)
-            img = imread(path_img)
+                for step in range(len(box_list)):
+                    _class = clustered[step]
+                    value = box_list[step]
+                    clusters_boxes[_class].append(value)
+
+            def _union_boxs_region():
+                nonlocal depleted_regions
+                for key in clusters_boxes:
+
+                    if key == -1:
+                        continue
+
+                    group = clusters_boxes.get(key)
+
+                    x = []
+                    y = []
+                    for (x0, x1, y0, y1) in group:
+                        x.append(x0)
+                        x.append(x1)
+                        y.append(y0)
+                        y.append(y1)
+
+                    x_max = int(max(x))
+                    x_min = int(min(x))
+                    y_max = int(max(y))
+                    y_min = int(min(y))
+
+                    depleted_regions.append([x_min, y_min, x_max, y_max])
+
+            _test_box_from_net()
+            _clustered_text_box()
+            _union_boxs_region()
+            if depleted_regions is not None and len(depleted_regions) != 0:
+                DBYaml.damp.text_box(boxes=depleted_regions,
+                                     path=DBYaml.from_image_path_to_yaml_path(
+                                             img_path))
+
+        if path_img is not None:
+            _get_text_box_easyocr(img_path=path_img)
+        else:
+            self._links_loader()
+            for img_name in self.img_names:
+                _img_path = self.image_directory + "/" + img_name
+                _get_text_box_easyocr(img_path=_img_path)
+
+    def get_text(self, path_img: str = None, method: str = "easyocr"):
+
+        def _preprocessing_img(img):
+
+            cv2.imshow("1", img)
+            cv2.waitKey()
+
+            return img
+
+        def easy_ocr_get_text(img_list: list):
+            text_list = []
+            for img in img_list:
+                img = _preprocessing_img(img)
+
+                bounds = reader.readtext(img)
+                text_in_box = []
+                for text in bounds:
+                    text_in_box.append(' ' + text[1])
+
+                text_list.append("".join(text_in_box))
+            return text_list
+
+        def _get_text():
+            path_yaml = DBYaml.from_image_path_to_yaml_path(path_img)
+            circumcised = ImageHandler.cut_text_boxes(
+                    path_yaml=path_yaml,
+                    path_img=path_img)
+
+            if circumcised is not False:
+                text = easy_ocr_get_text(circumcised)
+                DBYaml.damp.text(text_list=text,
+                                 path=path_yaml)
+            else:
+                print("регионы не вырезаны ")
+
+        if method == "easyocr":
+            reader = easyocr.Reader(['en'])
+            if path_img is not None:
+                _get_text()
+            else:
+                self._links_loader()
+                for img_name in self.img_names:
+                    path_img = self.image_directory + "/" + img_name
+                    _get_text()
 
     def text_correct(self):
         pass
@@ -91,11 +186,29 @@ class ImageHandler:
     def img_save(self):
         pass
 
+    @staticmethod
+    def draw_box(img_path, print_text: bool = False):
+        image = Image.open(img_path)
+        draw = ImageDraw.Draw(image)
+        boxes = DBYaml.load.text_box(
+                DBYaml.from_image_path_to_yaml_path(img_path))
+
+        for key in boxes:
+            bound = boxes.get(key)
+
+            draw.rectangle(bound, outline=(0, 0, 0))
+
+        return image.show()
+
 
 if __name__ == '__main__':
     start_time = time.time()
-    test = ImageHandler(image_directory=IMAGE_DIRECTORY,
-                        craft_net_path=CRAFT_PATH)
+    img_path = "example/4.jpg"
+    # ImageHandler.draw_box(img_path)
+
+    test = ImageHandler(image_directory=IMAGE_DIRECTORY)
     test.get_text_box()
-    print(test.img_names)
+    test.get_text()
+
     print("--- %s seconds ---" % (time.time() - start_time))
+    # ImageHandler.draw_box(img_path)
